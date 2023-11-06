62ab5cb99325827e2574cd923bf8ab8bb6005269
messbai24 <messbai2020@gmail.com>
2023-11-06 14:17:18 +0100

    bienvenidos

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo");
-		// TODO Auto-generated method stub
+		System.out.println("hola cabrones");
 
 	}
 
